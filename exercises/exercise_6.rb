af6ee02f3008ad4e1edb572e5b5f290544fa0a14
csaidane <cs.saidane@gmail.com>
2020-08-17 23:00:47 +0000

    activity complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 90)
+@store1.employees.create(first_name: "Test", last_name: "Mans", hourly_rate: 65)
+@store2.employees.create(first_name: "Orel", last_name: "Sonne", hourly_rate: 198)
+@store2.employees.create(first_name: "Whomstve", last_name: "Name", hourly_rate: 45)
+@store2.employees.create(first_name: "Creative", last_name: "Naming", hourly_rate: 80)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
 
+  has_many :employees
+  belongs_to :store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+
+
 end
